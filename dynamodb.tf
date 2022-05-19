2c94d7a1d44e10b58fdc321c1e517fce1d48598d
balumusham <105775706+balumusham@users.noreply.github.com>
2022-05-19 12:13:36 +0530

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,1 +1,14 @@
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock"{
+name = "infy-dynamodb-lock"
+hash_key = "LockID"
+read_capacity = 20
+write_capacity = 20
 
+attribute {
+name = "LockID"
+type = "S"
+}
+tags{
+Name = "DynamoDB Teraform state lock table"
+}
+}
